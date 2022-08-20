0605205fae4d760285df0dfec4fdf608b3eb2320
Naru Gopal <veeruk0217@gmail.com>
2022-08-20 20:08:46 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -21,120 +21,132 @@ function App() {
   const toSnakeCase = () => setData((a) => jsConvert.toSnakeCase(a));
   const toCamelCase = () => setData((a) => jsConvert.toCamelCase(a));
   const copyText = () => {
-    const textarea = document.getElementById('textarea');
+    const textarea = document.getElementById("textarea");
     textarea.select();
     document.execCommand("copy");
     navigator.clipboard.writeText(textarea.value);
   };
   return (
-    <section className="midsec">
-      <div className="container-lg">
-        <h1 className="display-5 mb-4 text-center text-white text-uppercase">
-          Case Converter
-        </h1>
-        <form>
-          <div className="">
-            <div className="mb-3">
-              <textarea
-                className="form-control"
-                value={data}
-                id="textarea"
-                rows="10"
-                onChange={inputChange}
-              ></textarea>
-            </div>
-            <div className="mt-3">
-              <div className="d-flex flex-wrap gap-2 justify-content-center mb-3 btnlist">
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toUpperCase}
-                >
-                  TO UPPERCASE
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toLowerCase}
-                >
-                  to lowercase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toSentenceCase}
-                >
-                  To sentencase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toKebabCase}
-                >
-                  to-kebab-case
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toHeaderCase}
-                >
-                  To Headercase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toTextCase}
-                >
-                  to textcase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toPathCase}
-                >
-                  to/pathcase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toDotCase}
-                >
-                  to.dotcase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toPascalCase}
-                >
-                  ToPascalCase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toSnakeCase}
-                >
-                  to_snake_case
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-secondary btn-sm"
-                  onClick={toCamelCase}
-                >
-                  toCamelCase
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success btn-sm"
-                  onClick={copyText}
-                >
-                  <i className="fas fa-copy mr-2"></i>Copy Text
-                </button>
+    <>
+      <section className="midsec">
+        <div className="container-lg">
+          <h1 className="display-5 mb-4 text-center text-white text-uppercase">
+            Case Converter
+          </h1>
+          <form>
+            <div className="">
+              <div className="mb-3">
+                <textarea
+                  className="form-control"
+                  value={data}
+                  id="textarea"
+                  rows="10"
+                  onChange={inputChange}
+                ></textarea>
+              </div>
+              <div className="mt-3">
+                <div className="d-flex flex-wrap gap-2 justify-content-center mb-3 btnlist">
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toUpperCase}
+                  >
+                    TO UPPERCASE
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toLowerCase}
+                  >
+                    to lowercase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toSentenceCase}
+                  >
+                    To sentencase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toKebabCase}
+                  >
+                    to-kebab-case
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toHeaderCase}
+                  >
+                    To Headercase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toTextCase}
+                  >
+                    to textcase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toPathCase}
+                  >
+                    to/pathcase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toDotCase}
+                  >
+                    to.dotcase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toPascalCase}
+                  >
+                    ToPascalCase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toSnakeCase}
+                  >
+                    to_snake_case
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary btn-sm"
+                    onClick={toCamelCase}
+                  >
+                    toCamelCase
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-success btn-sm"
+                    onClick={copyText}
+                  >
+                    <i className="fas fa-copy mr-2"></i>Copy Text
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
-      </div>
-    </section>
+          </form>
+        </div>
+      </section>
+      <footer className="footer">
+        <div className="container">
+          <p>
+            Made with 💖 | Design and developed by{" "}
+            <a href="https://iamnarugopal.in/" target="_blank">
+              Naru Gopal
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
 
